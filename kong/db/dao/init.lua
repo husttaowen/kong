961f55336ad32ce6e992afce2b49d4673cf57d33
Enrique García Cota <kikito@gmail.com>
2018-04-27 11:04:01 +0200

    wip - fix problem where custom daos could not override foreign key methods

--- kong/db/dao/init.lua
+++ kong/db/dao/init.lua
@@ -34,14 +34,14 @@ local DAO   = {}
 DAO.__index = DAO
 
 
-local function generate_foreign_key_methods(self)
-  local schema = self.schema
+local function generate_foreign_key_methods(schema)
+  local methods = {}
 
   for name, field in schema:each_field() do
     if field.type == "foreign" then
       local method_name = "for_" .. name
 
-      self[method_name] = function(self, foreign_key, size, offset)
+      methods[method_name] = function(self, foreign_key, size, offset)
         if type(foreign_key) ~= "table" then
           error("foreign_key must be a table", 2)
         end
@@ -90,13 +90,13 @@ local function generate_foreign_key_methods(self)
 
     elseif field.unique then
       local function validate_unique_value(unique_value)
-        local ok, err = self.schema:validate_field(field, unique_value)
+        local ok, err = schema:validate_field(field, unique_value)
         if not ok then
           error("invalid argument '" .. name .. "' (" .. err .. ")", 3)
         end
       end
 
-      self["select_by_" .. name] = function(self, unique_value)
+      methods["select_by_" .. name] = function(self, unique_value)
         validate_unique_value(unique_value)
 
         local row, err_t = self.strategy:select_by_field(name, unique_value)
@@ -111,7 +111,7 @@ local function generate_foreign_key_methods(self)
         return self:row_to_entity(row)
       end
 
-      self["update_by_" .. name] = function(self, unique_value, entity)
+      methods["update_by_" .. name] = function(self, unique_value, entity)
         validate_unique_value(unique_value)
 
         local entity_to_update, err = self.schema:process_auto_fields(entity, "update")
@@ -142,7 +142,7 @@ local function generate_foreign_key_methods(self)
         return row
       end
 
-      self["upsert_by_" .. name] = function(self, unique_value, entity)
+      methods["upsert_by_" .. name] = function(self, unique_value, entity)
         validate_unique_value(unique_value)
 
         local entity_to_upsert, err = self.schema:process_auto_fields(entity, "upsert")
@@ -173,7 +173,7 @@ local function generate_foreign_key_methods(self)
         return row
       end
 
-      self["delete_by_" .. name] = function(self, unique_value)
+      methods["delete_by_" .. name] = function(self, unique_value)
         validate_unique_value(unique_value)
 
         local entity, err, err_t = self["select_by_" .. name](self, unique_value)
@@ -195,16 +195,21 @@ local function generate_foreign_key_methods(self)
       end
     end
   end
+
+  return methods
 end
 
 
 function _M.new(db, schema, strategy, errors)
+  local fk_methods = generate_foreign_key_methods(schema)
+  local super      = setmetatable(fk_methods, DAO)
+
   local self = {
     db       = db,
     schema   = schema,
     strategy = strategy,
     errors   = errors,
-    super    = DAO,
+    super    = super,
   }
 
   if schema.dao then
@@ -214,9 +219,7 @@ function _M.new(db, schema, strategy, errors)
     end
   end
 
-  generate_foreign_key_methods(self)
-
-  return setmetatable(self, DAO)
+  return setmetatable(self, { __index = super })
 end
 
 
